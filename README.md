8acb95b1a968cd7bd98469e85b63df027aa8ad27
Chaz <chaz@plaidpotion.com>
2019-04-23 22:28:26 -0400

    wan wan

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# View Builder
+# View Builder - Playing with libtcod and python
 
-TUI Cassowary Editor
+Goal - make a constraint-based TUI editor for TUI apps.
 
-##
+## TODO
+ - Figure out why unit test for normalize_points is passing erroneously
+ - Translate autolayout.js to python, see if it works
+ - add linter
+
+## DONE
+ - Get cursor drag-to-draw-rectangle working in all four quadrants
+ - Add nose tests (or whatever is hot now in pyland)
+ - Clean up rendering during rectangle drawing
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import tcod
 import tcod.event
 
-from window import Window
 from app import App
+from util import normalize_points
+from window import Window
 
 # Setup the font.
 tcod.console_set_custom_font(
@@ -11,24 +12,16 @@ tcod.console_set_custom_font(
     tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,
 )
 
-def normalize_window(points, app):
-    print(points)
-    if points[1] < points[0]:
-        return app.make_window(points[1][0], points[1][1],
-                    points[0][0] - points[1][0], points[0][1] - points[1][1])
-    else:
-        return app.make_window(points[0][0], points[0][1],
-                    points[1][0] - points[0][0], points[1][1] - points[0][1])
-
 drawing = False
 global_points = []
-currentMousePoint = None
+current_mouse_point = None
 
 # Initialize the root console in a context.
 app = App()
 
 while True:
     tcod.console_flush()  # Show the console.
+    tcod.console_clear(app.root)  # Show the console.    
     for event in tcod.event.wait():
         if event.type == "QUIT":
             raise SystemExit()
@@ -38,22 +31,28 @@ while True:
         if event.type == "MOUSEBUTTONUP":
             # Create a proper window and persist it to app's children
             global_points.append(event.tile)
-            new_window = normalize_window(global_points, app)
+            new_window_points = normalize_points(global_points[0], current_mouse_point)
+            new_window = app.make_window(new_window_points[0][0],
+                                         new_window_points[0][1],
+                                         new_window_points[1][0] - new_window_points[0][0],
+                                         new_window_points[1][1] - new_window_points[0][1])
             new_window.persist()
 
             # Reset relevant state
             global_points = []
-            currentMousePoint = None
+            current_mouse_point = None
             drawing = False
         if event.type == "MOUSEMOTION" and drawing:
-            currentMousePoint = event.tile
+            current_mouse_point = event.tile
     for item in app.children:
         item.draw()
 
     # draw the current tile if we're drawing
-    if drawing and currentMousePoint:
-        currentWindow = normalize_window([global_points[0], currentMousePoint], app)
-        currentWindow.draw()
+    if drawing and current_mouse_point:
+        window_points  = normalize_points(global_points[0], current_mouse_point)
+        current_window = app.make_window(window_points[0][0], window_points[0][1],
+                                        window_points[1][0] - window_points[0][0], window_points[1][1] - window_points[0][1])
+        current_window.draw()
     
     # print the total number of widgets/rects/windows
     print(app.children)
