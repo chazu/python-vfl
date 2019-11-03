ebed17be2c75a47e4598316d60495f26efb047bc
Chaz <chaz@plaidpotion.com>
2019-11-02 22:49:58 -0400

    fix a couple omissions in previous refactor

--- main.py
+++ main.py
@@ -12,44 +12,68 @@ tcod.console_set_custom_font(
     tcod.FONT_LAYOUT_TCOD | tcod.FONT_TYPE_GREYSCALE,
 )
 
-drawing = False
-global_points = []
-current_mouse_point = None
+context = {
+    "current_mouse_point": None,
+    "drawing": False,
+    "global_points": []
+}
 
 # Initialize the root console in a context.
 app = App()
+drawing = False
+
+
+def handle_quit(context, event):
+    raise SystemExit()
+
+def handle_mouse_down(context, event):
+    context["global_points"].append(event.tile)
+    context["drawing"] = True
+
+def handle_mouse_move(context, event):
+    if context["drawing"]:
+        context["current_mouse_point"] = event.tile
+
+def handle_mouse_up(context, event):
+        # Create a proper window and persist it to app's children
+        context["global_points"].append(event.tile)
+        win_points = normalize_points(context["global_points"][0], context["current_mouse_point"])
+        new_window = app.make_window(win_points[0][0],
+                                     win_points[0][1],
+                                     win_points[1][0] - win_points[0][0],
+                                     win_points[1][1] - win_points[0][1])
+        new_window.persist()
+
+        # Reset relevant state
+        context["global_points"] = []
+        context["current_mouse_point"] = None
+        drawing = False
+
+handler_map = {
+    "QUIT": handle_quit,
+    "MOUSEBUTTONDOWN": handle_mouse_down,
+    "MOUSEBUTTONUP": handle_mouse_up,
+    "MOUSEMOTION": handle_mouse_move
+}
 
 while True:
     tcod.console_flush()  # Show the console.
     tcod.console_clear(app.root)  # Show the console.
-    for event in tcod.event.wait():
-        if event.type == "QUIT":
-            raise SystemExit()
-        if event.type == "MOUSEBUTTONDOWN":
-            global_points.append(event.tile)
-            drawing = True
-        if event.type == "MOUSEBUTTONUP":
-            # Create a proper window and persist it to app's children
-            global_points.append(event.tile)
-            win_points = normalize_points(global_points[0], current_mouse_point)
-            new_window = app.make_window(win_points[0][0],
-                                         win_points[0][1],
-                                         win_points[1][0] - win_points[0][0],
-                                         win_points[1][1] - win_points[0][1])
-            new_window.persist()
 
-            # Reset relevant state
-            global_points = []
-            current_mouse_point = None
-            drawing = False
-        if event.type == "MOUSEMOTION" and drawing:
-            current_mouse_point = event.tile
+    for event in tcod.event.wait():
+        print(event.type)
+        try:
+            handler = handler_map[event.type]
+            handler(context, event)
+        except KeyError as e:
+            print(f"unhandled event {e}")
+
     for item in app.children:
         item.draw()
 
     # draw the current tile if we're drawing
-    if drawing and current_mouse_point:
-        win_points  = normalize_points(global_points[0], current_mouse_point)
+    if drawing and context["current_mouse_point"]:
+        win_points  = normalize_points(context["global_points"][0], context["current_mouse_point"])
         cur_window = app.make_window(win_points[0][0],
                                      win_points[0][1],
                                      win_points[1][0] - win_points[0][0],
